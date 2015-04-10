06c8704931c2e144fa52c04b8cb27ab6e4cbd97a
jf712@ring <jf712@kmc.gr.jp>
2015-04-10 15:48:21 +0900

    to_h でいいや......

--- jf_nights_stream.rb
+++ jf_nights_stream.rb
@@ -1,4 +1,5 @@
 require 'tweetstream'
+require 'json'
 
 token = open('/home/jf712/.twitter/jf_nights').read.split("\n")
 
@@ -14,5 +15,25 @@ client = TweetStream::Client.new
 
 puts 'stream start!'
 client.userstream do |status|
-  puts status
+  
+  # 保存先
+  log_path = "/home/jf712/local/log/twitter/#{Time.now.strftime("%Y.%m.%d.json")}"
+  # 無かったら作る
+  open(log_path, 'w').close if !File.exists?(log_path)
+
+  # 保存してあるlog読み込み
+  json_data = open(log_path) do |io|
+    JSON.load(io)
+  end
+  # 無かったら作る
+  json_data = [] if json_data == nil
+
+  # なんかto_hしたらいい感じに見える
+  json_data << status.to_h
+
+  # 保存
+  open(log_path, 'w') do |io|
+    JSON.dump(json_data, io)
+  end
+  
 end
